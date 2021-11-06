4c2abd513c6703eac8f4b636e56ba64e39adde18
DESKTOP-E4UMQUR\bzic1 <bzic111@yandex.ru>
2021-11-06 11:35:41 +0300

    Clear

--- OOPHomework/Program.cs
+++ OOPHomework/Program.cs
@@ -4,35 +4,10 @@ namespace OOPHomework
 {
     class Program
     {
-        public enum AccountType
-        {
-            Debit,
-            Credit
-        }
-        class LightAccount
-        {
-            private int _id;
-            private decimal _balance;
-            private AccountType _accType;
-
-            public void SetId(int id) => _id = id;
-            public int GetId() => _id;
-            public void Enrollment(decimal sum) => _balance += sum;
-            public void Withdraw(decimal sum) => _balance -= sum;
-            public decimal GetBalance() => _balance;
-            public void SetAccType(AccountType accType) => _accType = accType;
-            public AccountType GetAccType() => _accType;
-        }
 
         static void Main(string[] args)
         {
-            LightAccount la = new LightAccount();
-
-            la.SetId(1);
-            la.SetAccType(AccountType.Debit);
-            la.Enrollment(1000);
-            la.Withdraw(100);
-            Console.WriteLine($"Account #: {la.GetId()}\nbalance : {la.GetBalance()}\nAccount Type : {la.GetAccType()}");
+            
         }
     }
 }
