03db3b087bda7ed12f6b6aacf0afe8342e573cb7
DESKTOP-E4UMQUR\bzic1 <bzic111@yandex.ru>
2021-11-09 19:00:55 +0300

    Ready

---
+++ OOPHomework/Atribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPHomework
+{
+    [AttributeUsage(AttributeTargets.Struct)]
+    class ExImAttribute : Attribute
+    {
+    }
+}
