7054bdd06b91fa09e77a4741ab70d13307a5e441
DESKTOP-E4UMQUR\bzic1 <bzic111@yandex.ru>
2021-11-12 20:27:05 +0300

    Начало

---
+++ OOPHomework/Figure.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPHomework
+{
+    public class Figure : IFigure
+    {
+        ConsoleColor Color { get; set; }
+        bool Visible { get; set; }
+        float _x { get; set; }
+        float _y { get; set; }
+
+        public string GetInfo()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void HorizontalMove(float x)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Move(float x, float y)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetColor(ConsoleColor color)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void VerticalMove(float y)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class Point : Figure
+    {
+
+    }
+}
---
+++ OOPHomework/Interface/IFigure.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPHomework
+{
+    internal interface IFigure
+    {
+        public void Move(float x, float y);
+        public void VerticalMove(float y);
+        public void HorizontalMove(float x);
+        public string GetInfo();
+        public void SetColor(ConsoleColor color);
+    }
+}
